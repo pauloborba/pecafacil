7654e8a1f54c17e5f810e4a0a7e193120b798667
Luiz Reis <luzrei@hotmail.com>
2017-12-04 04:15:39 -0300

    Implementado roteamento da gui

--- gui/pecafacil-gui/src/app/app.component.ts
+++ gui/pecafacil-gui/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
+
+  ngAfterViewInit() {
+    jQuery(".nav a").on("click", function(){
+      jQuery(".nav").find(".active").removeClass("active");
+      jQuery(this).addClass("active");
+    });
+  }
 }
--- gui/pecafacil-gui/src/app/app.module.ts
+++ gui/pecafacil-gui/src/app/app.module.ts
@@ -2,21 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-router';
 
 import { AppComponent } from './app.component';
 import { PratoComponent } from './prato/prato.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PratoComponent
+    PratoComponent,
+    LoginComponent
   ],
   imports: [
-    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
